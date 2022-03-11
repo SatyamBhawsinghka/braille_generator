7d79d1babd4295256164b7bb1f8a02c853fa33a1
Satyam Bhawsinghka <bhawsins@oregonstate.edu>
2022-03-10 23:35:32 -0800

    Testing speed

--- braille_generator.py
+++ braille_generator.py
@@ -82,7 +82,7 @@ class Motion(object):
 
 if __name__ == '__main__':
     motion = Motion()
-    motion.set_starts(0, -5, 15)
+    motion.set_starts(0, -4, 15)
     points = motion.get_xy()
 
     for i in points:
