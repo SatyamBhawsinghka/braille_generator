d4b5a9d60babbf3967df460178c5872ab55878d8
Satyam <bhawsins@oregonstate.edu>
2022-03-11 12:23:48 +0800

    try debugged

--- try.py
+++ try.py
@@ -18,21 +18,19 @@ from ArmIK.ArmMoveIK import *
 import HiwonderSDK.Board as Board
 from CameraCalibration.CalibrationConfig import *
 
+coord = [(-1,13,8),(0,13,8),(1,13,8)]
 AK = ArmIK()
-AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1000)
 time.sleep(1.5)
-time.sleep(5)
-AK.setPitchRangeMoving((0, 20, 10), -110, -30, -120, 1000)
-time.sleep(1)
-AK.setPitchRangeMoving((0, 20, 1), -110, -30, -120, 100)
-time.sleep(0.2)
-AK.setPitchRangeMoving((0.5, 20, 10), -110, -30, -120, 1000)
-time.sleep(1)
-AK.setPitchRangeMoving((0.5, 20, 1), -110, -30, -120, 100)
-time.sleep(0.2)
-AK.setPitchRangeMoving((1, 20, 10), -110, -30, -120, 1000)
-time.sleep(1)
-AK.setPitchRangeMoving((1, 20, 1), -110, -30, -120, 100)
-time.sleep(0.2)
-AK.setPitchRangeMoving((0, 10, 10), -30, -30, -120, 1000)
-time.sleep(1)
+for i in coord:
+    AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1000)
+    time.sleep(2)
+    AK.setPitchRangeMoving(i, -110, -30, -120, 1500)
+    time.sleep(2)
+    AK.setPitchRangeMoving((i[0],i[1],i[2]-8), -110, -30, -120, 100)
+    time.sleep(0.1)
+
+
+
+AK.setPitchRangeMoving((0,10,10), -110, -30, -120, 1500)
+time.sleep(2)
+
