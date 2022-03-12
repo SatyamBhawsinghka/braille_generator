286296a90ca36b23b866f03dc6242baab5047aab
Satyam Bhawsinghka <bhawsins@oregonstate.edu>
2022-03-11 22:43:08 -0800

    Increasing resolution

--- motion.py
+++ motion.py
@@ -16,7 +16,7 @@ from ArmIK.ArmMoveIK import *
 import HiwonderSDK.Board as Board
 
 class Motion(object):
-    def __init__(self, margin=np.array([3, 0, 1]), dx=np.array([1, 0, 1]), dy=np.array([0, -1, 1])):
+    def __init__(self, margin=np.array([1, 0, 1]), dx=np.array([0.5, 0, 1]), dy=np.array([0, -0.5, 1])):
         self.X = None
         self.Y = None
         self.AK = ArmIK()
